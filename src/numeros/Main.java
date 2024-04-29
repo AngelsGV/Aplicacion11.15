332a43645221ceebf849e2d6ca14708ffd9dfb9a
AngelsGV <angels.not99@gmail.com>
2024-04-29 11:31:02 +0200

    Initial commit

---
+++ src/numeros/Main.java
@@ -0,0 +1,17 @@
+package numeros;
+
+import static numeros.Metodos.*;
+
+// Crea un programa que lea números enteros desde el fichero numeros.dat y los vaya guardando en los ficheros pares.dat
+// e impares.dat según su paridad. Añade la función para mostrar los datos de los
+// ficheros pares.dat e impares.dat para comprobar que se han guardado correctamente.
+public class Main {
+    public static void main(String[] args) {
+
+        llenarArchivo();
+        separarNumeros();
+        mostrarFicheros();
+
+
+        }
+    }
